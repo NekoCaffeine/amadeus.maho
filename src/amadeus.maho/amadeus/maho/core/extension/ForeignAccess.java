0edca4a035e198408d06af3e386a7d74df40e6ed
NekoCaffeine <nekocaffeine@qq.com>
2023-02-24 16:44:21 +0800

    sync

--- src/amadeus.maho/amadeus/maho/core/extension/ForeignAccess.java
+++ src/amadeus.maho/amadeus/maho/core/extension/ForeignAccess.java
@@ -6,7 +6,7 @@ import amadeus.maho.transform.mark.base.TransformProvider;
 @TransformProvider
 public class ForeignAccess {
     
-    @Hook
-    private static Hook.Result implIsEnableNativeAccess(final Module $this) = Hook.Result.TRUE;
+    @Hook(forceReturn = true)
+    private static boolean implIsEnableNativeAccess(final Module $this) = true;
     
 }
--- src/amadeus.maho/amadeus/maho/lang/mark/StateSnapshot.java
+++ src/amadeus.maho/amadeus/maho/lang/mark/StateSnapshot.java
@@ -2,4 +2,9 @@ package amadeus.maho.lang.mark;
 
 // Used to mark this as a reversible AutoCloseable, currently used for unused var checking.
 @FunctionalInterface
-public interface StateSnapshot extends AutoCloseable { }
+public interface StateSnapshot extends AutoCloseable {
+    
+    @Override
+    void close();
+    
+}
