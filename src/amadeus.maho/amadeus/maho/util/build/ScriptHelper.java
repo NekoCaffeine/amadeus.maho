07838e4059ad6237c588ba5231c69dca897bdcce
NekoCaffeine <nekocaffeine@qq.com>
2023-09-02 19:00:03 +0800

    sync

--- src/amadeus.maho/amadeus/maho/util/build/ScriptHelper.java
+++ src/amadeus.maho/amadeus/maho/util/build/ScriptHelper.java
@@ -1,18 +1,12 @@
 package amadeus.maho.util.build;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import amadeus.maho.core.MahoExport;
 import amadeus.maho.core.MahoImage;
-import amadeus.maho.lang.SneakyThrows;
 import amadeus.maho.lang.inspection.Nullable;
 import amadeus.maho.util.misc.Environment;
 
@@ -29,77 +23,28 @@ public interface ScriptHelper {
             XX_DISABLE    = "-XX:-%s",
             ENABLE_ASSERT = "-ea";
     
+    String
+            ALL_DEFAULT     = "ALL-DEFAULT",
+            ALL_SYSTEM      = "ALL-SYSTEM",
+            ALL_MODULE_PATH = "ALL-MODULE-PATH";
+    
     String MAHO_JAVA_EXECUTION = "maho.java.execution";
     
     static void useMahoImageIfPossible() {
-       final @Nullable String image = System.getenv(MahoImage.VARIABLE);
-       if (image != null) {
-           final Path imagePath = Path.of(image);
-           if (Files.isDirectory(imagePath)) {
-               final Path java = imagePath / "bin" / (getOSType() == WINDOWS ? "java.exe" : "java");
-               if (Files.isRegularFile(java))
-                   Environment.local()[MAHO_JAVA_EXECUTION] = java.toAbsolutePath().toString();
-           }
-       }
+        final @Nullable String image = System.getenv(MahoImage.VARIABLE);
+        if (image != null) {
+            final Path imagePath = Path.of(image);
+            if (Files.isDirectory(imagePath)) {
+                final Path java = imagePath / "bin" / (getOSType() == WINDOWS ? "java.exe" : "java");
+                if (Files.isRegularFile(java))
+                    Environment.local()[MAHO_JAVA_EXECUTION] = java.toAbsolutePath().toString();
+            }
+        }
     }
     
     static void useDefaultImage() = Environment.local()[MAHO_JAVA_EXECUTION] = "java";
     
     static void useContextImage() = Environment.local()[MAHO_JAVA_EXECUTION] = (Path.of(System.getProperty("java.home")) / "bin" / "java").toAbsolutePath().toString();
-    
-    static Process run(final Workspace workspace, final Module module, final int debugPort = -1, final List<String> jvmArgs = List.of(), final boolean openTerminal = true,
-            final Path runDir = workspace.root() / module.path() / "run", final Predicate<Path> useModulePath = path -> true) = run(runDir, openTerminal, runArgs(workspace, module, debugPort, jvmArgs, useModulePath));
-    
-    static List<String> runArgs(final Workspace workspace, final Module module, final int debugPort = -1, final List<String> jvmArgs = List.of(), final Predicate<Path> useModulePath = path -> true) {
-        final ArrayList<String> args = { };
-        final ArrayList<Path> p = { }, cp = { };
-        args += Environment.local().lookup(MAHO_JAVA_EXECUTION, (Path.of(System.getProperty("java.home")) / "bin" / "java").toAbsolutePath().toString());
-        if (debugPort != -1)
-            args += "-agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=localhost:" + debugPort;
-        args *= jvmArgs;
-        p += (workspace.root() / workspace.output(Jar.MODULES_DIR, module)).toAbsolutePath();
-        module.dependencies().stream()
-                .filter(Module.SingleDependency::runtime)
-                .map(Module.SingleDependency::classes)
-                .forEach(path -> useModulePath.test(path) ? p : cp += path);
-        if (p.nonEmpty()) {
-            args += "-p";
-            args += p.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
-        }
-        if (cp.nonEmpty()) {
-            args += "-cp";
-            args += cp.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
-        }
-        args += "-m";
-        args += module.name();
-        return args;
-    }
-    
-    @SneakyThrows
-    static Process run(final Path directory = Path.of(""), final boolean openTerminal = true, final List<String> args) {
-        final ArrayList<String> commands = { };
-        if (openTerminal) {
-            final String command = String.join(" ", args);
-            switch (getOSType()) {
-                case WINDOWS -> commands *= List.of("cmd", "/c", "start", "/wait", "cmd", "/c", "%s & pause > nul".formatted(command));
-                case LINUX   -> commands *= List.of("xterm", "-e", "%s ; read -n 1 -s".formatted(command));
-                case MAC     -> commands *= List.of("osascript", "-e", "tell application 'Terminal' to do script '%s ; read -n 1 -s'".formatted(command));
-                default      -> throw unsupportedOSType();
-            }
-        } else
-            commands *= args;
-        return new ProcessBuilder()
-                .directory((~directory).toAbsolutePath().toFile())
-                .command(commands)
-                .start();
-    }
-    
-    static UnsupportedOperationException unsupportedOSType() = { "Unsupported OS Type: %s %s (%s)".formatted(System.getProperty("os.name"), System.getProperty("os.version"), System.getProperty("os.arch")) };
-    
-    String
-            ALL_DEFAULT     = "ALL-DEFAULT",
-            ALL_SYSTEM      = "ALL-SYSTEM",
-            ALL_MODULE_PATH = "ALL-MODULE-PATH";
     
     static void addModules(final Collection<String> args, final String... modules) = Stream.of(modules).forEach(module -> {
         args += "--add-modules";
--- src/amadeus.maho/amadeus/maho/util/build/Workspace.java
+++ src/amadeus.maho/amadeus/maho/util/build/Workspace.java
@@ -1,10 +1,19 @@
 package amadeus.maho.util.build;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import amadeus.maho.lang.EqualsAndHashCode;
+import amadeus.maho.lang.SneakyThrows;
 import amadeus.maho.lang.ToString;
 import amadeus.maho.util.config.Config;
+import amadeus.maho.util.misc.Environment;
+
+import static com.sun.jna.Platform.*;
 
 @ToString
 @EqualsAndHashCode
@@ -21,6 +30,59 @@ public record Workspace(Path root, Config config = Config.of(Config.Locator.ofFi
     public self flushMetadata(final BuildMetadata metadata = metadata().let(it -> it.buildCount++)) = config().save(metadata);
     
     public self clean(final Module module) = --(root() / module.path() / buildOutputDir());
+    
+    public Process run(final Module module, final int debugPort = -1, final List<String> jvmArgs = List.of(), final boolean openTerminal = true,
+            final Path runDir = root() / module.path() / "run", final Predicate<Path> useModulePath = path -> true)
+            = run(runDir, openTerminal, runArgs(module, debugPort, jvmArgs, useModulePath));
+    
+    public List<String> runArgs(final Module module, final int debugPort = -1, final List<String> jvmArgs = List.of(), final Predicate<Path> useModulePath = path -> true) {
+        final ArrayList<String> args = { };
+        final ArrayList<Path> p = { }, cp = { };
+        args += Environment.local().lookup(ScriptHelper.MAHO_JAVA_EXECUTION, (Path.of(System.getProperty("java.home")) / "bin" / "java").toAbsolutePath().toString());
+        if (debugPort != -1)
+            args += "-agentlib:jdwp=transport=dt_socket,server=n,suspend=y,address=localhost:" + debugPort;
+        args *= jvmArgs;
+        p += (root() / output(Jar.MODULES_DIR, module)).toAbsolutePath();
+        module.dependencies().stream()
+                .filter(Module.SingleDependency::runtime)
+                .map(Module.SingleDependency::classes)
+                .forEach(path -> useModulePath.test(path) ? p : cp += path);
+        if (p.nonEmpty()) {
+            args += "-p";
+            args += p.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
+        }
+        if (cp.nonEmpty()) {
+            args += "-cp";
+            args += cp.stream().map(Path::toString).collect(Collectors.joining(File.pathSeparator));
+        }
+        args += "-m";
+        args += module.name();
+        return args;
+    }
+    
+    @SneakyThrows
+    static Process run(final Path directory = Path.of(""), final boolean openTerminal = true, final List<String> args) {
+        final ArrayList<String> commands = { };
+        if (openTerminal) {
+            final String command = String.join(" ", args);
+            switch (getOSType()) {
+                case WINDOWS -> commands *= List.of("cmd", "/c", "start", "/wait", "cmd", "/c", "%s & pause > nul".formatted(command));
+                case LINUX   -> commands *= List.of("xterm", "-e", "%s ; read -n 1 -s".formatted(command));
+                case MAC     -> commands *= List.of("osascript", "-e", "tell application 'Terminal' to do script '%s ; read -n 1 -s'".formatted(command));
+                default      -> throw unsupportedOSType();
+            }
+        } else
+            switch (getOSType()) {
+                case WINDOWS -> commands *= List.of("cmd", "/c", String.join(" ", args));
+                default      -> commands *= args;
+            }
+        return new ProcessBuilder()
+                .directory((~directory).toAbsolutePath().toFile())
+                .command(commands)
+                .start();
+    }
+    
+    static UnsupportedOperationException unsupportedOSType() = { "Unsupported OS Type: %s %s (%s)".formatted(System.getProperty("os.name"), System.getProperty("os.version"), System.getProperty("os.arch")) };
     
     public static Workspace here(final Path buildDir = Path.of("build"), final Path outputDir = Path.of("output"))
             = { Path.of(""), Config.of(Config.Locator.ofFileSystem(buildDir)), buildDir, outputDir };
