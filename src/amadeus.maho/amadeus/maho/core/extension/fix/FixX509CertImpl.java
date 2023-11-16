aa5160f77cb8c15a7f89f3595f1a035ccb46620a
NekoCaffeine <nekocaffeine@qq.com>
2023-11-16 21:00:26 +0800

    fix: target missing

--- src/amadeus.maho/amadeus/maho/core/extension/fix/FixX509CertImpl.java
+++ src/amadeus.maho/amadeus/maho/core/extension/fix/FixX509CertImpl.java
@@ -8,11 +8,12 @@ import amadeus.maho.transform.mark.base.TransformProvider;
 
 import sun.security.x509.CertificateExtensions;
 import sun.security.x509.Extension;
+import sun.security.x509.X509CertImpl;
 
 @TransformProvider
 public interface FixX509CertImpl {
     
-    @Redirect(selector = "getExtensionValue", slice = @Slice(@At(method = @At.MethodInsn(name = "getExtension"))))
+    @Redirect(targetClass = X509CertImpl.class, selector = "getExtensionValue", slice = @Slice(@At(method = @At.MethodInsn(name = "getExtension"))))
     private static @Nullable Extension getExtension(final CertificateExtensions $this, final String extAlias) = $this?.getExtension(extAlias) ?? null;
     
 }
