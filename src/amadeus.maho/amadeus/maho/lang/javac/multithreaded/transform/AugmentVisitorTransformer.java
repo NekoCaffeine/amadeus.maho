291ca955c1fe173bf48adb0553b0e00f8cfba49a
NekoCaffeine <nekocaffeine@qq.com>
2024-05-09 04:33:53 +0800

    add missing class

---
+++ src/amadeus.maho/amadeus/maho/lang/javac/multithreaded/transform/AugmentVisitorTransformer.java
@@ -0,0 +1,30 @@
+package amadeus.maho.lang.javac.multithreaded.transform;
+
+import java.security.ProtectionDomain;
+import java.util.Set;
+
+import org.objectweb.asm.tree.ClassNode;
+
+import amadeus.maho.lang.inspection.Nullable;
+import amadeus.maho.transform.ClassTransformer;
+import amadeus.maho.transform.mark.base.Transformer;
+import amadeus.maho.util.bytecode.context.TransformContext;
+
+import static org.objectweb.asm.Opcodes.ACC_SYNCHRONIZED;
+
+@Transformer
+public class AugmentVisitorTransformer implements ClassTransformer.Limited {
+    
+    @Override
+    public @Nullable ClassNode transform(final TransformContext context, @Nullable final ClassNode node, @Nullable final ClassLoader loader, @Nullable final Class<?> clazz, @Nullable final ProtectionDomain domain) {
+        context.markModified();
+        node.methods.stream()
+                .filter(method -> method.name.equals("augment"))
+                .forEach(method -> method.access |= ACC_SYNCHRONIZED);
+        return node;
+    }
+    
+    @Override
+    public Set<String> targets() = Set.of("com.sun.tools.javac.code.Lint$AugmentVisitor");
+    
+}
